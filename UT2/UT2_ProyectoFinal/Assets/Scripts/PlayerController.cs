950e5eb715d1c75a191f33b113a6312c84734b37
MichaelGarciaJ <mgarcia271476@gmail.com>
2024-10-31 01:02:24 +0100

    Movimiento + Salto + Giro + Suelo + Anima
    Movimiento hecho con la física de velocidad.
    Salto hecho con RayCast.
    Giro del personaje hecho.
    Suelo para probar con tiles hecho.
    Animaciones faltan, sobre todo usar el Blend Tree.

--- UT2/UT2_ProyectoFinal/Assets/Scripts/PlayerController.cs
+++ UT2/UT2_ProyectoFinal/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,102 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    Transform transform;
-    public float horizontalVelocity = 0.06f;
-    public float verticalVelocity = 0.04f;
-    
+    Rigidbody2D rb;
+    public float velocidad;
+    public float jumpForce;
+    public LayerMask groundLayer;
+    public float rayLenght;
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    Animator animator;
+
     void Start()
     {
-        transform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-   
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + Input.GetAxis("Horizontal") * horizontalVelocity;
-        position.y = position.y + Input.GetAxis("Vertical") *  verticalVelocity;
-        transform.position = position;
+        // Se encarga el movimiento de derecha a izquierda. 
+        procesarMovimiento_Giro_Animacion();
+
+        // Se encarga el movimiento del salto.
+        procesarSalto();
+
     }
+
+    // Método en el que gestionamos el movimiento y giro del jugador. En nuestro caso lo vamos a hacer con velocity + GetAxis.
+    void procesarMovimiento_Giro_Animacion()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(inputMovimiento * velocidad, rb.velocity.y);
+
+        //Se encarga de gestionar el giro del jugador.
+        gestionarGiro(inputMovimiento);
+        rb.velocity = new Vector2(inputMovimiento * velocidad, rb.velocity.y);
+
+        //Se encarga de gestionar si el jugador esta quieto o no.
+        gestionarMovimiento(inputMovimiento);
+        rb.velocity = new Vector2(inputMovimiento * velocidad, rb.velocity.y);
+    }
+
+    // Método en el que se encarga en el salto del jugador, también tenemos en cuenta la gravedad. 
+    //En nuestro caso lo vamos a hacer con Raycast.
+    void procesarSalto()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLenght, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && hit.collider != null)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < 0) // Se encarga de aumentar la gravedad cuando el jugador esta cayendo.
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) //Este caso sale cuando el jugador salta.
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    // Método en el que gestionamos el giro del jugador.
+    void gestionarGiro(float inputMovimiento)
+    {
+
+        if (inputMovimiento > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (inputMovimiento < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    // Método en el que se verifica si el jugador esta quieto o no (Para la animación).
+    void gestionarMovimiento(float inputMovimiento)
+    {
+
+        rb.velocity = new Vector2(inputMovimiento * velocidad, rb.velocity.y);
+
+        if (inputMovimiento != 0) // Está en movimiento.
+        {
+            animator.SetBool("enMovimiento", true);
+        }
+        else // Este caso sale cuando el jugaor esta quieto.
+        {
+            animator.SetBool("enMovimiento", false);
+        }
+    }
+
+    // Método para visualizar el Raycast en la escena.
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * rayLenght);
+    }
+
 }
